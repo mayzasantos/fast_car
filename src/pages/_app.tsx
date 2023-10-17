86e664760d733ba70c5643bd80df4bbd973144ff
Mayza <mayza.santos@outlook.com.br>
2023-10-17 15:04:32 -0300

    initial organization

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { AppProps } from "next/app";
+
+export default function App({Component, pageProps}: AppProps){
+    return(
+        <>
+        <h1>HomePage</h1>
+        </>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,55 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors:{
+      brand:{
+        1:"#4529E6",
+        2:"#5126EA",
+        3:"#B0A6F0",
+        4:"#EDEAFD",
       },
-    },
+
+      grey:{
+        0:"#0B0D0D",
+        1:"#212529",
+        2:"#495057",
+        3:"#868E96",
+        4:"#ADB5BD",
+        5:"#CED4DA",
+        6:"#DEE2E6",
+        7:"#E9ECEF",
+        8:"#F1F3F5",
+        9:"#F8F9FA",
+        10:"#FDFDFD",
+        whiteFixed:"#FFFFFF"
+      },
+
+      feedback:{
+        alert_1:"#CD2B31",
+        alert_2: "#FDD8D8",
+        alert_3: "#FFE5E5",
+        sucess_1:"#18794E",
+        sucess_2:"#CCEBD7",
+        sucess_3:"#DDF3E4"
+      },
+
+      random:{
+        random_1:"#E34D8C",
+        random_2:"#C04277",
+        random_3:"#7D2A4D",
+        random_4:"#7000FF",
+        random_5:"#6200E3",
+        random_6:"#36007D",
+        random_7:"#349974",
+        random_8:"#2A7D5F",
+        random_9:"#153D2E",
+        random_10:"#6100FF",
+        random_11:"#5700E3",
+        random_12:"#30007D",
+      },
+
+     
+    }
   },
   plugins: [],
 }
